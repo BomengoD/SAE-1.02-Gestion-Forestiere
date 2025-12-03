9ebcc722ceec3ccfc1e8edf8aa9d7363783c5e05
Ruben Boussa <ruben.boussa@gmail.com>
2025-12-03 14:17:35 +0100

    Problème de la première ligne et de l'affichage corrigé. Le problème reste le chargement dans la structure.

--- main.c
+++ main.c
@@ -30,28 +30,27 @@ passés par pointeur
 void lirecharger_fichier(const char *nom_fichier, int *nb_arbres, TABLE tab_arbres){
     FILE* fichier = NULL;
     char chaine[TAILLE_MAX];
-
+     int i = 0;
     fichier = fopen(nom_fichier, "r");
     if(fichier != NULL){
-        /*On compte le nombre d'arbres gràace à fgets*/
-        while (fgets(chaine, TAILLE_MAX, fichier) != NULL){
-            printf("%s", chaine);
-            (*nb_arbres)++;
-        }
-        tab_arbres.arbres = (ARBRE*) malloc (*nb_arbres * sizeof(ARBRE));// allocation d'un espace mémoire pour le tableau dynamique d'arbres
-        rewind(fichier); // repositionner le curseur au début du fichier
-        int i = 0;
-        while (fgets(chaine, TAILLE_MAX, fichier) != NULL){
-            sscanf(chaine, "%s;%s;%d;%d;%f;%f;%f", &tab_arbres.arbres[i].identifiant,
-            &tab_arbres.arbres[i].espece, &tab_arbres.arbres[i].age, &tab_arbres.arbres[i].sante,
-            &tab_arbres.arbres[i].hauteur, &tab_arbres.arbres[i].diametre, &tab_arbres.arbres[i].volume);
-            i++;
-            
+        fgets(chaine, sizeof(chaine), fichier); // lire la première ligne (en-tête) et l'ignorer !
+        /*On compte le nombre d'arbres grâce à fgets, fgets parcours chaque ligne une par une donc chaque itération est une ligne*/
+        while (fgets(chaine, sizeof(chaine), fichier) != NULL){
+            (*nb_arbres)++; // incrémenter le compteur d'arbres pour chaque ligne lue
+            char *champ = strtok(chaine, ";"); // découpage de la ligne en champs par le séparateur ";"
+            int champ_num = 0; // compteur de champs
+            printf("Données de l'arbre %d:\n", *nb_arbres);
+            while (champ && champ_num < 7){// boucle pour afficher chaque champ tant qu'il y a des champs et que le nombre de champs est inférieur à 7(qui est le nombre d'attributs d'un arbre donc le nombre de colonnnes dans le fichier csv)
+                printf("    Champ %d: %s\n", champ_num + 1, champ); // affichage du numéro du champ et son contenu
+                champ = strtok(NULL, ";");// obtenir le champ suivant, NULL indique de continuer à partir de la position actuelle
+                champ_num++; 
+            }
         }
         fclose(fichier);
     }else{
         printf("Erreur d'ouverture du fichier %s\n", nom_fichier);
     }
+    tab_arbres.arbres = (ARBRE*) malloc((*nb_arbres) * sizeof(ARBRE)); // allocation dynamique de la mémoire pour stocker les arbres
   
 }
 
@@ -62,11 +61,6 @@ int main(){
     TABLE tab_arbres;
     lirecharger_fichier(nom_fichier, &nb_arbres, tab_arbres);
     printf("\n Nombre d'arbres lus: %d\n", nb_arbres);
-    for (int i = 0; i < nb_arbres; i++){
-        printf(" %s; %s; %d ; %d; %.2f; %.2f; %.2f\n", tab_arbres.arbres[i].identifiant, tab_arbres.arbres[i].espece,
-        tab_arbres.arbres[i].age, tab_arbres.arbres[i].sante, tab_arbres.arbres[i].hauteur,
-        tab_arbres.arbres[i].diametre, tab_arbres.arbres[i].volume);
-    }
     free(tab_arbres.arbres);
     return 0;
     
